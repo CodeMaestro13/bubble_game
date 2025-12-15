e32cc41ec489acc62e270912a98edb84eece38ac
CodeMaestro13 <krishnanarde2001@gmail.com>
2025-12-15 17:13:18 +0530

    added high score feature

--- script.js
+++ script.js
@@ -3,9 +3,12 @@
 
 // for generating the bubbles we are using for loop 
 
-var timer=3;
+var timer=30;
 var hitrn;
 var score=0;
+var highscore=localStorage.getItem("highScore") || 0 ;
+document.querySelector("#highScore").innerHTML=highscore;
+
 function makeBubble(){
     var clutter=" ";
 for(var i=0;i<=139;i++){
@@ -41,10 +44,26 @@ function getNewHit(){
 function increaseScore(){
     score +=10;
     document.querySelector("#score").innerHTML=score;
+
+    // logic for highscore
+    if(score>highscore){
+        highscore=score;
+        localStorage.setItem("highScore",highscore);
+        document.querySelector("#highScore").innerHTML=highscore;
+    }
 }
 
+// function for high score
+
+// function highScore(){
+//     if(score>=highScore){
+//         highscore+=score;
+//         document.querySelector("#highScore").innerHTML=highscore;
+//     }
+// }
+
 // concept :-- event bubbling in javascript :
-// --> 
+// --> agar ek element pe event call kiya or if its not work on that then uske parent pr event call hoga or udhar bhi nahi to parent ke parent pr call hoga and its go on--in on 
 
 document.querySelector("#pbtm")
 .addEventListener("click", function(details){
@@ -53,10 +72,10 @@ document.querySelector("#pbtm")
         increaseScore();
         makeBubble();
         getNewHit();
-
     }
 });
 
 makeBubble();
 runTimer();
 getNewHit();
+// highScore();
