dde593c51ce71dd3544ec897548df2a86e78d032
CodeMaestro13 <krishnanarde2001@gmail.com>
2025-12-15 18:11:18 +0530

    Added Animations in bubble generation

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 // for generating the bubbles we are using for loop 
 
-var timer=30;
+var timer=35;
 var hitrn;
 var score=0;
 var highscore=localStorage.getItem("highScore") || 0 ;
@@ -53,15 +53,6 @@ function increaseScore(){
     }
 }
 
-// function for high score
-
-// function highScore(){
-//     if(score>=highScore){
-//         highscore+=score;
-//         document.querySelector("#highScore").innerHTML=highscore;
-//     }
-// }
-
 // concept :-- event bubbling in javascript :
 // --> agar ek element pe event call kiya or if its not work on that then uske parent pr event call hoga or udhar bhi nahi to parent ke parent pr call hoga and its go on--in on 
 
@@ -75,7 +66,53 @@ document.querySelector("#pbtm")
     }
 });
 
+gsap.from(".bubble", {
+    scale: 0,
+    duration: 0.5,
+    stagger: 0.02
+  });
+
+
+// document.querySelector("#pbtm").addEventListener("click", function(e) {
+//     if(e.target.classList.contains("bubble")) {
+
+//         gsap.to(e.target, {
+//             scale: 0,
+//             duration: 0.2,
+//             ease: "power2.out"
+//         });
+
+//     }
+// });
+
+gsap.from("#score", {
+    scale: 1.5,
+    duration: 0.3,
+    ease: "bounce.out"
+  }); 
+  gsap.from("#hitval", {
+    scale: 1.5,
+    duration: 0.3,
+    ease: "bounce.out"
+  }); 
+  gsap.from("#timerval", {
+    scale: 1.5,
+    duration: 0.3,
+    ease: "bounce.out"
+  }); 
+  gsap.from("#highScore", {
+    scale: 1.5,
+    duration: 0.3,
+    ease: "bounce.out"
+  });   
+
+
 makeBubble();
+//  this is generating the bubble code by animation
+gsap.from(".bubble", {
+    scale: 0,
+    duration: 0.5,
+    stagger: 0.02
+  });
 runTimer();
 getNewHit();
-// highScore();
